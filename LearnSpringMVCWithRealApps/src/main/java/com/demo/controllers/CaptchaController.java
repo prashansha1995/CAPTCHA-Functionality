2318b267692d0929eba0e41f62888b85edb00bf0
Prashansha Gupta <prashanshagupta5991@gmail.com>
2020-06-30 14:32:47 +0530

    Adding method logic to Generate CAPTCHA/Commenting n code refactoring

--- LearnSpringMVCWithRealApps/src/main/java/com/demo/controllers/CaptchaController.java
+++ LearnSpringMVCWithRealApps/src/main/java/com/demo/controllers/CaptchaController.java
@@ -3,6 +3,7 @@ package com.demo.controllers;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
@@ -22,17 +23,23 @@ import java.awt.image.BufferedImage;
 @RequestMapping("/captcha")
 public class CaptchaController {
 	
-	@RequestMapping(method = RequestMethod.GET,value="/def")
-	//public String index() {
-
+	/**
+	 * This method will set the CAPTCHA to decorated text.It will draw string of CAPTCHA generated to image.
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
+	@RequestMapping(method = RequestMethod.GET,value="/generateCaptcha")
 	public void index(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("image/jpg");
-		int iTotalChars = 6;
+		Random randChars = new Random();
+		int lengthOfCaptcha= 6;
+		String captcha=getRandomGeneratedCaptcha(lengthOfCaptcha,randChars);
 		int iHeight = 40;
 		int iWidth = 150;
 		Font fntStyle1 = new Font("Arial", Font.BOLD, 30);
-		Random randChars = new Random();
-		String sImageCode = (Long.toString(Math.abs(randChars.nextLong()), 36)).substring(0, iTotalChars);
 		BufferedImage biImage = new BufferedImage(iWidth, iHeight, BufferedImage.TYPE_INT_RGB);
 		Graphics2D g2dImage = (Graphics2D) biImage.getGraphics();
 		int iCircle = 15;
@@ -40,20 +47,38 @@ public class CaptchaController {
 			g2dImage.setColor(new Color(randChars.nextInt(255), randChars.nextInt(255), randChars.nextInt(255)));
 		}
 		g2dImage.setFont(fntStyle1);
-		for (int i = 0; i < iTotalChars; i++) {
+		for (int i = 0; i < lengthOfCaptcha; i++) {
 			g2dImage.setColor(new Color(randChars.nextInt(255), randChars.nextInt(255), randChars.nextInt(255)));
 			if (i % 2 == 0) {
-				g2dImage.drawString(sImageCode.substring(i, i + 1), 25 * i, 24);
+				g2dImage.drawString(captcha.substring(i, i + 1), 25 * i, 24);
 			} else {
-				g2dImage.drawString(sImageCode.substring(i, i + 1), 25 * i, 35);
+				g2dImage.drawString(captcha.substring(i, i + 1), 25 * i, 35);
 			}
 		}
 		OutputStream osImage = response.getOutputStream();
 		ImageIO.write(biImage, "jpeg", osImage);
 		g2dImage.dispose();
 		HttpSession session = request.getSession();
-		session.setAttribute("captcha_security", sImageCode);
-		//return "CAPTCHA";
+		session.setAttribute("captcha_security", captcha);
+	}
+    /**
+     * This method will return the String of random Generated CAPTCHA of desired length.
+     * @param lengthOfCaptcha
+     * @param randChars
+     * @return
+     */
+	private String getRandomGeneratedCaptcha(int lengthOfCaptcha,Random randChars) {
+		
+		StringBuffer captchaBuffer=new StringBuffer();
+		IntStream.rangeClosed(1, lengthOfCaptcha).forEach(length ->
+		{
+			int charEle=0;
+			int getRandomChar=Math.abs(randChars.nextInt())%62;
+			charEle=getRandomChar < 26 ? 65+getRandomChar :getRandomChar<52 ? 97+(getRandomChar-26): 48+(getRandomChar-52) ;
+			captchaBuffer.append((char)charEle);
+		}
+				);
+		return captchaBuffer.toString();
 	}
 
 }
--- LearnSpringMVCWithRealApps/src/main/webapp/index.jsp
+++ LearnSpringMVCWithRealApps/src/main/webapp/index.jsp
@@ -1,55 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
     <%@ page isELIgnored="false" %>
-<%-- <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>   --%>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-   
-      <script>
-   
-/* $(document).ready(function() 
-		{
-	alert("hello");
-	$.ajax({
-	    url:'${pageContext.request.contextPath}/captcha/def',
-	    success: function(a) {
-	    	alert("success");
-
-	    }
-
-	  });
-    
-  
-  }); */
-  
-  
-
-/* function captcha()
-{
-	alert("abc");
-	$.ajax({
-
-	    type: "GET",
-        dataType: 'json',
-       contentType:'application/json',
-	    url:'${pageContext.request.contextPath}/captcha/abc',
-	    success: function(a) {
-
-	       if(a.error==null)
-
-	{
-
-
-	}
-
-	    }
-
-	  });
-	} */
-</script>
-
 </head>
 
 <body>
@@ -59,7 +14,7 @@
 
 				<td>Captcha</td>
 				<td>
-				   <img src="${pageContext.request.contextPath}/captcha/def">
+				   <img src="${pageContext.request.contextPath}/captcha/generateCaptcha">
 					<br>
 					<input type="text" name="captcha" required="required" style="margin-top: 5px;">
 					<br>
